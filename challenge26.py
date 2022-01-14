73941018e29ee566e2f243235765c837ca54053d
Rogue1-cmd <garipeter01@gmail.com>
2022-01-14 17:51:26 +0300

    factor generator

---
+++ challenge26.py
@@ -0,0 +1,32 @@
+#Factor generator app
+import time
+print("Welcome to the factor generator app")
+flag = True
+
+
+while flag:
+    x = int(input("Enter number to find factors for: \t"))
+    factors = []
+    for i in range(1, x+1):
+        if (x%i) == 0:
+            factors.append(i)
+
+    print("The factors of ",x,"are:")
+    for factor in factors:
+        print(factor)
+
+    time.sleep(2) 
+
+    print ("\nIn Summary: \n")
+    for i in range(int(len(factors)/2)):
+        print (factors[i],"*",factors[-i-1], "=", (x))  
+
+    time.sleep(2)
+
+    prompt = input("Would you like to try again ?(y/n): \n").lower()
+    if prompt != "y":
+        flag = False
+        print("Okay bye")
+
+
+
