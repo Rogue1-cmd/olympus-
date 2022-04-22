a5e2d97a8eaddcf04b3b8548aa9ec81c70d13357
Rogue1-cmd <garipeter01@gmail.com>
2022-04-22 12:10:18 +0300

    Fisher-Yates Shuffle Algorithm

---
+++ yates.py
@@ -0,0 +1,16 @@
+#Fisher_Yates Shuffle Algorithm
+import random
+
+#num = list(map(int, input("Enter Multiple integer values to shuffle :\n\t").split())) >>> help out with this!!!!
+
+num = [34, 45, 78, 90, 12, 3, 5]
+
+print("Original List: ", str(num))
+
+for i in range(len(num)-1, 0, -1):
+
+    j = random.randint(0, i+1) #pick random between 0, i
+
+    num[i], num[j] = num[j], num[i] #swap arr[i] with element at random index
+
+print("Shuffled list:", str(num))
